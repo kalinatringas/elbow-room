b9f728a8a6e96617827bc3e294371ee34d14c7a8
lillyxliu <lilly.liu.panda@gmail.com>
2026-03-09 14:47:29 -0400

    Created Posting Form Component
    - Created UI Form
    -- Text input
    -- Submit Button
    -- Validation

--- front-end/app/(tabs)/index.tsx
+++ front-end/app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 import { View,Text } from 'react-native';
+import PostForm from '@/components/PostForm';
 
 export default function HomeScreen() {
+  function handlePost(text:string){
+    console.log("New post:", text);
+  }
   return (
    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-purple-500">
         Welcome to PURPLE!
       </Text>
+   
+      <PostForm onSubmit={handlePost} />
     </View>
   );
-}
-
+} 
