01a8b4f0ff414d9640e262a7f555aeca78b44f83
kalinatringas <kalinatringas@ufl.edu>
2026-03-30 13:43:47 -0400

    refactored code to upload properly

--- back-end/main.py
+++ back-end/main.py
@@ -1,5 +1,5 @@
 # All libs that are needed
-from fastapi import FastAPI, HTTPException, Header, Depends, UploadFile, File, Body, Query
+from fastapi import FastAPI, HTTPException, Header, Depends, UploadFile, File, Body, Query, Form
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
@@ -130,19 +130,16 @@ def get_posts(user=Depends(get_current_user)):
         post["liked_by_me"] = any(l["user_id"] == str(user.id) for l in likes)
     return posts
 
-@app.post("/upload-avatar")
-async def upload_avatar(
-    file: UploadFile = File(...),
-    user = Depends(get_current_user),
-):
-    print("filename:", file.filename)
-    print("content_type:", file.content_type)
+
+async def process_and_upload_avatar(
+    file: UploadFile,
+    user_id: str,   
+)->str:
 
     if file.content_type not in ALLOWED_TYPES:
         raise HTTPException(status_code=400, detail="Invalid file type: only JPEG, PNG, and WEBP allowed")
     
     contents = await file.read()
-    print("size:", len(contents))
 
     # checking if file bigger than 2mb
     if (len(contents) > MAX_SIZE_MB * 1024 * 1024):
@@ -160,7 +157,7 @@ async def upload_avatar(
         raise HTTPException(status_code=500, detail=f"image processing failed: {str(e)}")
 
     try:
-        file_path = f'{user.id}/avatar.jpeg'
+        file_path = f'{user_id}/avatar.jpeg'
         supabase_admin.storage.from_("avatars").upload(
         path=file_path,
         file=buffer.read(),
@@ -172,12 +169,45 @@ async def upload_avatar(
     public_url = supabase.storage.from_('avatars').get_public_url(file_path)
     supabase.table("profiles").update({"avatar_url":public_url}).eq("id", user.id).execute()
 
-    return {"avatar_url": public_url}
+    return public_url
+@app.post("/upload-avatar")
+async def upload_avatar_route(file:UploadFile = File(...),
+                              user = Depends(get_current_user)):
+    url = await process_and_upload_avatar(file, user.id)
+    supabase.table("profiles").update({"avatar_url": url}).eq("id", user.id).execute()
+    return {"avatar_url": url}
 
-@app.patch("/users/:id")
-async def update_user(patch: UpdateUser, file: UploadFile = File(...), user=Depends(get_current_user)):
-    supabase.table("profiles").update({"username":patch.username}).eq("id", user.id).execute()
-    supabase.table("profiles").update({"bio":patch.bio}).eq("id", user.id).execute()
-    supabase.table("profiles").update({"name":patch.name}).eq("id", user.id).execute()
-    upload_avatar(file)
-    return {}
+
+@app.patch("/users/me")
+async def update_user(username: str = Form(None),
+                      bio: str = Form(None),
+                      name: str = Form(None), 
+                      file: UploadFile = File(None), 
+                      user=Depends(get_current_user)):
+    updates = {}
+
+    # i used form fields here instead of a pydantic model since we have bote files + text fields (Upload avatar)
+    
+    if username is not None: 
+        updates["username"] = username
+    if bio is not None:
+        updates["bio"] = bio
+    if name is not None:
+        updates["name"] = name
+
+    if file is not None:
+        avatar_url = await process_and_upload_avatar(file, user_id = user.id)
+        updates["avatar_url"] = avatar_url
+
+    if not updates:
+        raise HTTPException(status_code=400, detail="No fields to update")
+    
+    result = supabase.table("profiles").update(updates).eq("id", user.id).execute()
+
+    # supabase.table("profiles").update({"username":patch.username}).eq("id", user.id).execute()
+    # supabase.table("profiles").update({"bio":patch.bio}).eq("id", user.id).execute()
+    # supabase.table("profiles").update({"name":patch.name}).eq("id", user.id).execute()
+    # upload_avatar(file)
+    if not result.data:
+        raise HTTPException(status_code=500, detail="Update failed")
+    return result.data[0]
